17e115f675dd30ff5719ab2689d6cccef06a2999
dhappy05 <dhappy05@126.com>
2017-05-23 14:20:52 +0800

    test

--- demo1-helloworld/src/main/java/com/example/demo/controller/HelloController.java
+++ demo1-helloworld/src/main/java/com/example/demo/controller/HelloController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String index(){
+
         return "Hello World";
     }
 }

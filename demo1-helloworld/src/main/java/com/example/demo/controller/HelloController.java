da6c4877ac48d561a991fc85733b995c4803b11c
dhappy05 <dhappy05@126.com>
2017-05-23 14:12:27 +0800

    Initial commit

---
+++ demo1-helloworld/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by ding on 2017/5/22.
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String index(){
+        return "Hello World";
+    }
+}

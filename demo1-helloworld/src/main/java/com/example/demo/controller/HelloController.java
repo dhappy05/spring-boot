e4a50d50dfd8775c5a4955bede5f6eab5822bbe3
dhappy05 <dhappy05@126.com>
2017-05-23 14:23:00 +0800

    删除空行

--- demo1-helloworld/src/main/java/com/example/demo/controller/HelloController.java
+++ demo1-helloworld/src/main/java/com/example/demo/controller/HelloController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String index(){
-
         return "Hello World";
     }
 }
